2ab4da7aa34861d77400fa4097871ea533d228e9
FetenFeyrer <72172103+FetenFeyrer@users.noreply.github.com>
2023-08-10 11:29:08 +0200

    endgame

---
+++ REFACTORED/compute_pca_xeofs.py
@@ -0,0 +1,28 @@
+import numpy as np
+from xeofs.xarray import EOF
+import varimax_xeofs as rot
+import xarray as xr
+# input: a data set (n x 36(=p)), (number of components computed))
+#
+# returns: PC loadings (nx36)
+def compute_pca(data, n_components=6, correlation_mode=False):
+    
+
+    
+    model = EOF(data, norm=True, dim=['time'])
+    model.solve()
+    
+   
+    eofs = model.eofs()
+    
+
+    explained_variances = model.explained_variance_ratio()
+
+    pcs = model.pcs()
+    #print(loadings.shape)
+
+    #varimax rotation
+    #rot_eofs, rot_explained_variances = rot.varimax_xeofs(model)
+    
+    
+    return pcs, eofs, eofs, explained_variances*100, explained_variances*100
